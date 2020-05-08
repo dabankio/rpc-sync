9c439c10a1ab53b1d5049fba006fe65d8c6dbef2
zhongjin <chajiubu@hotmail.com>
2020-05-08 11:39:27 +0800

    data sync by rpc

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+#host="112.126.60.134"
+#port=3306
+#user="bbc"
+#password="BBC@$1024bg#"
+#db="bigbang-dpos"
+#forkid="00000000b0a9be545f022309e148894d1e1c853ccac3ef04cb6f5e5c70f41a70"
+
+host="127.0.0.1"
+port=3306
+user="bbc"
+password="BBC@$1024bg#"
+db="bigbang-dpos"
+forkid="00000000b0a9be545f022309e148894d1e1c853ccac3ef04cb6f5e5c70f41a70"
