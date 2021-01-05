26b904662c1a824af3a62e1ac3880490d6f5b037
shangqd <shang_qd@qq.com>
2021-01-05 19:50:11 +0800

    vote 2

--- vote/weight.py
+++ vote/weight.py
@@ -8,6 +8,7 @@ import time, datetime
 from decimal import Decimal
 import sys
 import os
+sys.path.append("..")
 import config
 
 connection = pymysql.connect(host=config.host, port=config.port, user=config.user, password=config.password, db=config.db)
@@ -74,14 +75,17 @@ def Export(height):
     data = []
     f = open(('./%d.json' % height), 'w')    
     with connection.cursor() as cursor:
-        cursor.execute("select addr,(a1 - a2) as a,(v1 - v2) as v from Vote where (v1 - v2) > 0 or (a1 - v2) > 0")
+        cursor.execute("select addr,(a1 - a2) as a,(v1 - v2) as v from Vote")
         rows = cursor.fetchall()
         for r in rows:
-            data.append({
-                "address": r[0],
-                "balance": float(r[1]),
-                "vote": float(r[2]),
-                "height": height})
+            if r[1] > 0 or r[2] > 0:
+                data.append({
+                    "address": r[0],
+                    "balance": float(r[1]),
+                    "vote": float(r[2])})
+            if r[1] < 0 or r[2] < 0:
+                print("err:",r)
+                sys.exit()
     f.write(json.dumps(data))
     f.close()
     print("Export OK")
